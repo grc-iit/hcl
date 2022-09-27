1bb6258b5e360a4d3e37e307f8eb709c455c2085
aparna sasidharan <aparnasasidharan2017@gmail.com>
2022-09-27 02:20:13 -0400

    concurrent queues

---
+++ include/hcl/concurrent/queue/queue.h
@@ -0,0 +1,170 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Distributed under BSD 3-Clause license.                                   *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Illinois Institute of Technology.                        *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of Hermes. The full Hermes copyright notice, including  *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the COPYING file, which can be found at the top directory. If you do not  *
+ * have access to the file, you may request a copy from help@hdfgroup.org.   *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef INCLUDE_HCL_QUEUE_CONCURRENT_H_
+#define INCLUDE_HCL_QUEUE_CONCURRENT_H_
+
+/**
+ * Include Headers
+ */
+
+#include <hcl/common/debug.h>
+#include <hcl/common/singleton.h>
+#include <hcl/communication/rpc_factory.h>
+#include <hcl/communication/rpc_lib.h>
+/** MPI Headers**/
+#include <mpi.h>
+/** RPC Lib Headers**/
+#ifdef HCL_ENABLE_RPCLIB
+
+#include <rpc/client.h>
+#include <rpc/rpc_error.h>
+#include <rpc/server.h>
+
+#endif
+/** Thallium Headers **/
+#if defined(HCL_ENABLE_THALLIUM_TCP) || defined(HCL_ENABLE_THALLIUM_ROCE)
+#include <thallium.hpp>
+#endif
+
+/** Boost Headers **/
+#include <boost/algorithm/string.hpp>
+#include <boost/lockfree/queue.hpp>
+/** Standard C++ Headers**/
+#include <hcl/common/container.h>
+
+#include <functional>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+#include <float.h>
+
+namespace hcl {
+
+template <class ValueT>
+class queue_concurrent : public container 
+{
+
+public:
+	typedef boost::lockfree::queue<ValueT> 	queue_type;
+private:
+	queue_type *queue;
+
+
+ public:
+
+  ~queue_concurrent() 
+  {
+    if(queue != nullptr) delete queue;
+    this->container::~container(); 
+  }
+
+  void construct_shared_memory() override 
+  {
+
+  }
+  void open_shared_memory() override 
+  {
+
+  }
+  void bind_functions() override 
+  {
+    switch (HCL_CONF->RPC_IMPLEMENTATION) {
+#ifdef HCL_ENABLE_RPCLIB
+      case RPCLIB: {
+        std::function<bool(ValueT &)> pushFunc(
+            std::bind(&queue_concurrent<ValueT>::LocalPush, this,
+                      std::placeholders::_1));
+        std::function<bool(ValueT &)> popFunc(
+            std::bind(&queue_concurrent<ValueT>::LocalPop, this));
+
+        rpc->bind(func_prefix + "_Push", pushFunc);
+        rpc->bind(func_prefix + "_Pop", popFunc);
+        break;
+      }
+#endif
+#ifdef HCL_ENABLE_THALLIUM_TCP
+      case THALLIUM_TCP:
+#endif
+#ifdef HCL_ENABLE_THALLIUM_ROCE
+      case THALLIUM_ROCE:
+#endif
+#if defined(HCL_ENABLE_THALLIUM_TCP) || defined(HCL_ENABLE_THALLIUM_ROCE)
+      {
+
+        std::function<void(const tl::request &, ValueT &)> pushFunc(
+	   std::bind(&queue_concurrent<ValueT>::ThalliumLocalPush,
+           this, std::placeholders::_1,std::placeholders::_2));
+        std::function<void(const tl::request &)> popFunc(
+            std::bind(&queue_concurrent<ValueT>::ThalliumLocalPop,
+                      this, std::placeholders::_1));
+
+        rpc->bind(func_prefix + "_Push", pushFunc);
+        rpc->bind(func_prefix + "_Pop", popFunc);
+        break;
+      }
+#endif
+    }
+  }
+
+  explicit queue_concurrent(CharStruct name_ = "TEST_QUEUE_CONCURRENT",uint16_t port = HCL_CONF->RPC_PORT)
+      : container(name_, port)
+  {
+    queue = nullptr;
+    AutoTrace trace = AutoTrace("hcl::queue_concurrent");
+    if (is_server) 
+    {
+      queue = new queue_type (1024);
+      construct_shared_memory();
+      bind_functions();
+    } 
+    else if (!is_server && server_on_node) 
+    {
+      open_shared_memory();
+    }
+  }
+
+  queue_type *data() 
+  {
+     return queue;
+  }
+
+  bool LocalPush(ValueT &v)
+  {
+     return queue->push(v);
+  }
+  std::pair<bool,ValueT> LocalPop()
+  {
+      ValueT v;
+      bool b = queue->pop(v);
+      if(b) return std::pair<bool,ValueT> (true,v);
+      else return std::pair<bool,ValueT> (false,ValueT());
+  }
+
+#if defined(HCL_ENABLE_THALLIUM_TCP) || defined(HCL_ENABLE_THALLIUM_ROCE)
+  THALLIUM_DEFINE(LocalPush, (v), ValueT& v)
+  THALLIUM_DEFINE1(LocalPop)
+#endif
+
+   bool Push(uint64_t &s, ValueT& v);
+   std::pair<bool,ValueT> Pop(uint64_t &s);
+
+};
+
+#include "queue.cpp"
+
+}  // namespace hcl
+
+#endif  
---
+++ test/concurrent_queue_test.cpp
@@ -0,0 +1,132 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Distributed under BSD 3-Clause license.                                   *
+ * Copyright by The HDF Group.                                               *
+ * Copyright by the Illinois Institute of Technology.                        *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of Hermes. The full Hermes copyright notice, including  *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the COPYING file, which can be found at the top directory. If you do not  *
+ * have access to the file, you may request a copy from help@hdfgroup.org.   *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <execinfo.h>
+#include <hcl/common/data_structures.h>
+#include <hcl/concurrent/queue/queue.h>
+#include <mpi.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <chrono>
+#include <functional>
+#include <iostream>
+#include <utility>
+#include <random>
+
+int main(int argc, char *argv[]) {
+  int provided;
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+  if (provided < MPI_THREAD_MULTIPLE) {
+    printf("Didn't receive appropriate MPI threading specification\n");
+    exit(EXIT_FAILURE);
+  }
+  int comm_size, my_rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  int ranks_per_server = comm_size, num_request = 100;
+  long size_of_request = 1000;
+  bool debug = false;
+  bool server_on_node = false;
+  if (argc > 1) ranks_per_server = atoi(argv[1]);
+  if (argc > 2) num_request = atoi(argv[2]);
+  if (argc > 3) size_of_request = (long)atol(argv[3]);
+  if (argc > 4) server_on_node = (bool)atoi(argv[4]);
+  if (argc > 5) debug = (bool)atoi(argv[5]);
+
+  /* if(comm_size/ranks_per_server < 2){
+       perror("comm_size/ranks_per_server should be atleast 2 for this test\n");
+       exit(-1);
+   }*/
+  int len;
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+  MPI_Get_processor_name(processor_name, &len);
+  if (debug) {
+    printf("%s/%d: %d\n", processor_name, my_rank, getpid());
+  }
+
+  if (debug && my_rank == 0) {
+    printf("%d ready for attach\n", comm_size);
+    fflush(stdout);
+    getchar();
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+  bool is_server = (my_rank + 1) % ranks_per_server == 0;
+  int my_server = my_rank / ranks_per_server;
+  int num_servers = comm_size / ranks_per_server;
+
+  // The following is used to switch to 40g network on Ares.
+  // This is necessary when we use RoCE on Ares.
+  std::string proc_name = std::string(processor_name);
+  /*int split_loc = proc_name.find('.');
+  std::string node_name = proc_name.substr(0, split_loc);
+  std::string extra_info = proc_name.substr(split_loc+1, string::npos);
+  proc_name = node_name + "-40g." + extra_info;*/
+
+  size_t size_of_elem = sizeof(int);
+
+  printf("rank %d, is_server %d, my_server %d, num_servers %d\n", my_rank,
+         is_server, my_server, num_servers);
+
+  const int array_size = TEST_REQUEST_SIZE;
+
+  if (size_of_request != array_size && my_rank==0) {
+    printf(
+        "Please set TEST_REQUEST_SIZE in include/hcl/common/constants.h "
+        "instead. Testing with %d\n",
+        array_size);
+  }
+
+  HCL_CONF->IS_SERVER = is_server;
+  HCL_CONF->MY_SERVER = my_server;
+  HCL_CONF->NUM_SERVERS = num_servers;
+  HCL_CONF->SERVER_ON_NODE = server_on_node || is_server;
+  HCL_CONF->SERVER_LIST_PATH = "./server_list";
+
+  hcl::queue_concurrent<int> *queue = new hcl::queue_concurrent<int> ();
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Comm client_comm;
+  MPI_Comm_split(MPI_COMM_WORLD, !is_server, my_rank, &client_comm);
+  int client_comm_size;
+  MPI_Comm_size(client_comm, &client_comm_size);
+  
+  std::default_random_engine rd;
+  std::uniform_int_distribution<int> dist(0,100000000);
+
+  rd.seed(my_rank);
+  auto die = std::bind(dist,rd);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  if (!is_server) 
+  {
+
+      for (int i = 0; i < num_request; i++) 
+      {
+        int value = dist(rd)%10000;
+        uint64_t s = random()%num_servers;
+        queue->Push(s,value);
+      }
+      
+      for (int i = 0; i < num_request; i++) 
+      {
+	uint64_t s = random()%num_servers;
+	std::pair<bool,int> r = queue->Pop(s);
+      }
+  }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  delete (queue);
+  MPI_Finalize();
+  exit(EXIT_SUCCESS);
+}
